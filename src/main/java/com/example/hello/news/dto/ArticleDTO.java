f3ea0ff2a0c622aa304985b732c43320c43c5fe8
hong991204 <hong991204@naver.com>
2025-09-27 09:24:34 +0900

    first commit

---
+++ src/main/java/com/example/hello/news/dto/ArticleDTO.java
@@ -0,0 +1,23 @@
+package com.example.hello.news.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor      // 매개변수들이 없는 생성자 코드를 생성
+@AllArgsConstructor
+@Builder
+public class ArticleDTO {
+    private SourceDTO source;
+    private String author;
+    private String title;
+    private String description;
+    private String url;
+    private String urlToImage;
+    private String publishedAt;
+    private String content;
+
+
+}
